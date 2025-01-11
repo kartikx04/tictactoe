d7c91c10eef2be939a9c947d7404051c683a15bd
Siddharth <kartikx04@gmail.com>
2025-01-11 21:46:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Board from "./components/Board.jsx"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <Board />
+    </>
+  )
+}
+
+export default App
