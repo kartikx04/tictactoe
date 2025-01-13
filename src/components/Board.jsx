480059303b9855b97e097024f66e3f5cd853f7e8
kartik Verma <kartikx04@gmail.com>
2025-01-14 02:42:15 +0530

    updates

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -69,14 +69,38 @@ function calculateWinner(squares) {
 
 export default function Game() {
     const [xIsNext, setXIsNext] = useState(true);
-    const [history, setHistory] = useState([Array(9).fill(null)])
-    const currentSquares = history[history.length - 1]
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [currentMove, setCurrentMove] = useState(0);
+    const currentSquares = history[currentMove];
 
     function handlePlay(nextSquares) {
-        setHistory([...history, nextSquares])
-        setXIsNext(!xIsNext)
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1);
+        setXIsNext(!xIsNext);
+    }
+
+    function jumpTo(nextMove) {
+        setCurrentMove(nextMove);
+        setXIsNext(nextMove % 2 === 0);
     }
     
+    const moves = history.map((squares, move) => {
+
+        let description;
+        if (move > 0) {
+            description = "Go to move #" + move;
+        } else {
+            description = "Go to game beginning"
+        }
+
+        return (
+            <li key={move}>
+                <button className="history-btn" onClick={()=> jumpTo(move)} >{description}</button>
+            </li>
+        )
+    })
+
     return (
         <div className="game">
             <div className="game-board">
@@ -84,7 +108,7 @@ export default function Game() {
             </div>
             <div className="game-info">
                 <ol>
-                    {/* TODO */}
+                    {moves}
                 </ol>
             </div>
         </div>
